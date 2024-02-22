43a19ba3545bafd778a00ed3799aab012b9e9486
Wendel Paes <107985828+WendelPaesx@users.noreply.github.com>
2024-02-22 18:50:41 -0300

    configurando aws ´pra enviar email

--- src/Componentes/AssinarNewsLetter/index.jsx
+++ src/Componentes/AssinarNewsLetter/index.jsx
@@ -1,21 +1,18 @@
 import React from "react";
 import styled from "styled-components";
 import { Box, Typography, TextField, Button } from "@mui/material";
+import AWS from "aws-sdk";
 
 const StyledBox = styled(Box)`
-  
   display: flex;
   align-items: center;
-  
-  
-  
 `;
 
 const ContentBox = styled(Box)`
   width:35%;
   height: 60%;
   position: absolute;
-  top: 30%;
+  top: 30%;   
   left: 10%;
   z-index: 2;
   background-color: rgba(245, 245, 245, 0.5);
@@ -53,70 +50,118 @@ const LogoImage = styled.img`
 `;
 
 export default function AssinarNewsLetter() {
-  const handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(event.target.email.value);
-  };
-  return (
-    <StyledBox>
-      <ContentBox>
-        <Typography
-          variant="body1"
-          component="div"
-          sx={{
-            fontFamily: "Noto Serif, Roboto",
-            color: "black",
-            fontSize: "1.9em",
-            lineHeight: "0",
-            
-          }}
-        >
-          Sua casa com as
-          <Typography
-            variant="h1"
-            component="h1"
-            sx={{ color: "black", fontSize: "10vh", lineHeight: "1.3",  }}
-          >
-            Melhores plantas
-          </Typography>
-        </Typography>
-        
-        <Typography variant="body1" component="p" sx={{ color: 'black', fontSize: '1.5em', lineHeight: '1' , }}>
-          Encontre aqui uma vasta seleção de plantas para decorar a sua casa e torná-la um lugar mais feliz no seu dia a dia.
-        </Typography>
-        <Typography variant="body1" component="p" sx={{ color: 'black', fontSize: '1.5em', lineHeight: '1' , marginTop:'1em'}}>
-          Inscreva-se em nossa newsletter para receber novidades sobre a marca.
-        </Typography>
-        
-        
-        <form onSubmit={handleSubmit}>
-          <TextField
-            id="email"
-            name="email"
-            type="email"
-            label="Insira seu e-mail"
-            variant="outlined"
-            fullWidth
-            sx={{ marginBottom: '1em', marginTop:'1em'}}
-          />
-          <Button type="submit" variant="contained" color="warning" sx={{ fontSize: '1em', height: '3em', cursor: 'pointer' }}>
-            Assinar Newsletter
-          </Button>
-        </form>
+  const handleSubmit = async (event) => {
+     event.preventDefault()   
+    
+     const email = event.target.email;
+     
 
-       
-        
-      </ContentBox>
-      <LogoImage
-        src="img/imagem-hero1.png"
-        alt="Planta"
-        
-      />
-      <BackgroundImage
-        src="img/Vector.png"
-        alt="vector"
-       
-      />
-    </StyledBox>
-  );
-}
+
+const ses = new AWS.SES();
+
+    //config do aws ses
+    AWS.config.update({
+      AWS_ACCESS_KEY_ID:"AKIAYS2NU2UBWINPN34W",
+      AWS_SECRET_ACCESS_KEY:"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",
+      AWS_REGION:"sa-east-1",
+    });
+    
+    // Parâmetros do e-mail
+    const params = {
+      Source: "wendel_wop2@hotmail.com", // Endereço de e-mail do remetente
+      Destination: {
+        ToAddresses: [email], // Endereço de e-mail do destinatário
+      },
+      Message: {
+        Subject: {
+          Data: "Confirmação de Cadastro", // Assunto do e-mail
+        },
+        Body: {
+          Text: {
+            Data: "Olá, você se cadastrou em nossa newsletter. Obrigado!", // Corpo do e-mail
+          },
+        },
+      },
+    };
+   
+      try {
+        // Enviar e-mail usando SES
+        const result = await ses.sendEmail(params).promise();
+        console.log("E-mail enviado com sucesso!", result);
+      } catch (error) {
+        console.error("Erro ao enviar e-mail:", error);
+      }
+  };
+    
+
+    return (
+      <StyledBox>
+        <ContentBox>
+          <Typography
+            variant="body1"
+            component="div"
+            sx={{
+              fontFamily: "Noto Serif, Roboto",
+              color: "black",
+              fontSize: "1.9em",
+              lineHeight: "0",
+            }}
+          >
+            Sua casa com as
+            <Typography
+              variant="h1"
+              component="h1"
+              sx={{ color: "black", fontSize: "10vh", lineHeight: "1.3" }}
+            >
+              Melhores plantas
+            </Typography>
+          </Typography>
+
+          <Typography
+            variant="body1"
+            component="p"
+            sx={{ color: "black", fontSize: "1.5em", lineHeight: "1" }}
+          >
+            Encontre aqui uma vasta seleção de plantas para decorar a sua casa e
+            torná-la um lugar mais feliz no seu dia a dia.
+          </Typography>
+          <Typography
+            variant="body1"
+            component="p"
+            sx={{
+              color: "black",
+              fontSize: "1.5em",
+              lineHeight: "1",
+              marginTop: "1em",
+            }}
+          >
+            Inscreva-se em nossa newsletter para receber novidades sobre a
+            marca.
+          </Typography>
+
+          <form >
+            <TextField
+              id="email"
+              name="email"
+              type="email"
+              label="Insira seu e-mail"
+              variant="outlined"
+              fullWidth
+              sx={{ marginBottom: "1em", marginTop: "1em" }}
+            />
+            <Button onClick={handleSubmit}
+              type="submit"
+              variant="contained"
+              color="warning"
+              sx={{ fontSize: "1em", height: "3em", cursor: "pointer" }}
+            >
+              Assinar Newsletter
+            </Button>
+          </form>
+        </ContentBox>
+        <LogoImage src="img/imagem-hero1.png" alt="Planta" />
+        <BackgroundImage src="img/Vector.png" alt="vector" />
+      </StyledBox>
+    );
+  };
+
